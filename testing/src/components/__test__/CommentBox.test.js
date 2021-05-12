8c8946a531708b51caa2b393f59d722021190587
Federico Rodriguez <fede90.12@gmail.com>
2021-05-12 17:55:14 -0300

    test in submit form

--- testing/src/components/__test__/CommentBox.test.js
+++ testing/src/components/__test__/CommentBox.test.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { mount } from "enzyme";
 import CommentBox from "components/CommentBox";
-import { unmountComponentAtNode } from "react-dom";
 
 let wrapped;
 
@@ -13,7 +12,40 @@ afterEach(() => {
   wrapped.unmount();
 });
 
+// Test: El componente tiene un textarea y un boton?
 it("has a text area and a button", () => {
   expect(wrapped.find("textarea").length).toEqual(1);
   expect(wrapped.find("button").length).toEqual(1);
+});
+
+// El usuario puede escribir en el textarea?
+it("has a text area that users can type in", () => {
+  // simulate(event[, mock]) => self
+  wrapped.find("textarea").simulate("change", {
+    target: { value: "new comment" },
+  });
+  // upadte Re-renderiza el componente
+  wrapped.update();
+
+  // prop(key) nos permite extraer el valor que se pasa dentro de cualquier
+  // elemento dentro del componente.
+  expect(wrapped.find("textarea").prop("value")).toEqual("new comment");
+});
+
+it("when form is submited, text area gets emptied", () => {
+  // simulate(event[, mock]) => self
+  wrapped.find("textarea").simulate("change", {
+    target: { value: "new comment" },
+  });
+  // upadte Re-renderiza el componente
+  wrapped.update();
+
+  // busca el form, y simula el envio del formulario
+  wrapped.find("form").simulate("submit");
+
+  // actualiza el componente
+  wrapped.update();
+
+  // busca el text area y mira si esta vacio
+  expect(wrapped.find("textarea").prop("value")).toEqual("");
 });
